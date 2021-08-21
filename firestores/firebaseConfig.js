4d698f4f777c3785452bf0fed9d49170ddb4e951
riverkim <dev.riverkim@gmail.com>
2021-08-21 15:29:00 +0900

    fix config setting

---
+++ firestores/firebaseConfig.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STOTAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
+
+  //   apiKey: "AIzaSyDUgBc9QuTdoywJsz-KTyghsU6Y4Bi0s98",
+  //   authDomain: "sns-project-b9393.firebaseapp.com",
+  //   projectId: "sns-project-b9393",
+  //   storageBucket: "sns-project-b9393.appspot.com",
+  //   messagingSenderId: "644241623859",
+  //   appId: "1:644241623859:web:52412b109778dd272fcc46",
+  //   measurementId: "G-140M94Q8CV",
+};
+
+export default firebaseConfig;
